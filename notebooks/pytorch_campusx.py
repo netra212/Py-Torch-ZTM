ce5465d3bf22699f021b65acfa0d3d2e53fcab5a
netr212 <netra200021kcbdr@gmail.com>
2025-01-05 20:22:29 +0000

    Pytorch

---
+++ notebooks/pytorch_campusx.py
@@ -0,0 +1,19 @@
+import torch
+
+# Tensor -> Data Structure or Multi-dimensional array designed for
+# mathematical and computational efficiency.
+
+
+# Real World Examples:
+# 1. Scalar: 0-dimensional tensors (a single number)
+# -> Represents a single value, often used for simple metrics or constants.
+# -> Example:- Loss value: After forward pass, the loss function computes a
+# single scalar value indicatign the difference between the predictred and
+# actual ouputs.
+
+# 
+
+# 
+
+
+
